80472ebb45a14a3d4c10cd8d3fb6d6a3a2de6dc5
Carlos Mendoza <carlos_mendoza2@hotmail.com>
2023-08-23 22:31:12 -0400

    Donwloaded necessary libraries, created foundational components and routing. File structure has been updated.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Header from "./Components/Header/Header";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import CalendarPage from "./pages/CalendarPage/CalendarPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/my-calendar" element={<CalendarPage/>}/>
+          <Route path="/my-profile" element={<ProfilePage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
